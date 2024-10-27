e74e3629585f4487114d3cfe8463518dd941cfb4
jamilshehab <jamilshehab>
2024-10-27 19:35:02 +0200

    files added

---
+++ numberpyramids.py
@@ -0,0 +1,8 @@
+#enter the user input 
+user_input=int(input("enter the number : "))
+# total=0
+#loop through the user and add the user input + 1 
+for user in range(1,user_input+1):
+ for usertwo in range(1,user+1):
+     print(usertwo,end="")
+ print()
---
+++ readme.md
@@ -0,0 +1,8 @@
+Write a Python program that prompts the user for a number, x, and displays a pyramid of numbers. Each row in the pyramid should contain increasing numbers, starting from 1 up to x. Example: 
+
+Enter x: 5
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
