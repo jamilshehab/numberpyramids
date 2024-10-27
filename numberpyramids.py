5f5f3ab0ed918075ef9e0bd2484a61d860a12b8e
jamilshehab <jamilshehab>
2024-10-27 20:13:48 +0200

    added files

--- numberpyramids.py
+++ numberpyramids.py
@@ -3,6 +3,7 @@ user_input=int(input("enter the number : "))
 # total=0
 #loop through the user and add the user input + 1 
 for user in range(1,user_input+1):
- for usertwo in range(1,user+1):
+  for usertwo in range(1,user+1):
      print(usertwo,end="")
- print()
+     #print a new line 
+  print()
